45559934ba8df7db38d39667dc6c23ea619cfbfa
sharad_mourya <sharadmourya27@gmail.com>
2023-01-09 18:59:59 +0530

    implement middleware and integrate create data api

--- frontend/pages/singleManga/_id.vue
+++ frontend/pages/singleManga/_id.vue
@@ -32,9 +32,9 @@
             </div>
             <div class="btn d-flex">
               <button class="bg-blue-500 hover:bg-blue-600 text-white font-medium text-sm px-4 py-2 rounded dark:bg-gray-700 dark:text-blue-400 hover:border-blue-400 ml-5" @click="openPdf">Read Now</button>
-              <button class="bg-blue-500 hover:bg-blue-600 text-white font-medium text-sm px-4 py-2 rounded dark:bg-gray-700 dark:text-blue-400 hover:border-blue-400 ml-5" @click="handleDownload(getSingleManga.file)">Download</button>
+              <button class="bg-blue-500 hover:bg-blue-600 text-white font-medium text-sm px-4 py-2 rounded dark:bg-gray-700 dark:text-blue-400 hover:border-blue-400 ml-5">Download</button>
               <div class="update-section ml-4">
-                <small class="update-text">Want to Update this book? <button data-modal-target="defaultModal" data-modal-toggle="defaultModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="handleUpdate">Update</button>
+                <small class="update-text">Want to Update this book? <button class="bg-blue-500 hover:bg-blue-600 text-white font-medium text-sm px-4 py-2 rounded dark:bg-gray-700 dark:text-blue-400 hover:border-blue-400 ml-5" @click="handleUpdate">Update</button>
                 </small>
               </div>
              
@@ -43,6 +43,7 @@
         </div>
       </div>
     </div>
+   
     <div id="openpdf" class="card-content card flex items-center shadow-xl" v-if="pdfShow">
       <button class="bg-blue-500 hover:bg-blue-600 text-white font-medium text-sm px-4 py-2 rounded dark:bg-gray-700 dark:text-blue-400 hover:border-blue-400 ml-5" v-if="pdfShow" @click="closePdf">Stop Reading</button>
        <OpenPdf :pdfSource="getSingleManga.file" />
